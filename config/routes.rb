ce7cc958113eed8ea7378993970517e15c417303
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-02 17:09:45 -0500

    fix display_name bug when registering a new user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :facts
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   devise_scope :user do
     get 'users/sign_up' => 'registrations#new'
