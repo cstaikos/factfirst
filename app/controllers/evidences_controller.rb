abd0cf50982ea62e008cfb6b6014927783e929b2
Chris Staikos <cstaikos@gmail.com>
2016-03-03 10:29:21 -0500

    Update score every time a vote is cast

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -9,6 +9,7 @@ class EvidencesController < ApplicationController
       end
     else
       Vote.create(upvote: true, user_id: current_user.id, evidence_id: @evidence.id)
+      @evidence.fact.update_score
 
       respond_to do |format|
         format.js {}
@@ -25,6 +26,7 @@ class EvidencesController < ApplicationController
       end
     else
       Vote.create(upvote: false, user_id: current_user.id, evidence_id: @evidence.id)
+      @evidence.fact.update_score
 
       respond_to do |format|
         format.js {}
