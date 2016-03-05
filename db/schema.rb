61a9c19263208030873d6a3fa461e59db08dba81
Chris Staikos <cstaikos@gmail.com>
2016-03-05 18:42:44 -0500

    Remove gravatar hash from user model, simply calculate each time via model method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305231446) do
+ActiveRecord::Schema.define(version: 20160305234059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20160305231446) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "gravatar_hash"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
