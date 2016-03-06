1afec39fb211e4c533ffe69567f593ee1ed89cb5
Filip Stankovic <filips@gmail.com>
2016-03-06 10:18:48 -0500

    Fix bug where homeNav js function was running on every page

--- app/assets/javascripts/refills/navigation.js
+++ app/assets/javascripts/refills/navigation.js
@@ -12,34 +12,3 @@ $(document).on("ready page:load", function() {
     });
   });
 });
-
-// Switch nav bars on home page when scrolling gets to a certain point
-function homeNavDisplay() { $(function() {
-  $(document).bind('ready page:load scroll', function() {
-	   	var docScroll = $(document).scrollTop();
-      // This is something we might want for mobile later in the below if statement: & $('html').height() > 768 -- ie. no sticky header for short screens
-		  if (docScroll >= 325) {
-
-        if ($('.img-logo').hasClass('nodisplay')) {
-          $('.img-logo').removeClass('nodisplay');
-        }
-
-        if ($('nav').hasClass('nodisplay')) {
-          $('nav').removeClass('nodisplay');
-        }
-
-        if (!$('.navigation').hasClass('nav-bg')) {
-          $('.navigation').addClass('nav-bg');
-        }
-	  	} else {
-		  	$('.navigation').removeClass('nav-bg');
-        if (!$('.img-logo').hasClass('nodisplay')) {
-          $('.img-logo').addClass('nodisplay');
-        }
-        if (!$('nav').hasClass('nodisplay')) {
-          $('nav').addClass('nodisplay');
-        }
-	  	}
-    });
-  });
-};
