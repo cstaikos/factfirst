8577eda01bd6d1dcf088b209c3af0bf7874e7fc5
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-05 22:32:49 -0500

    write tests for Evidence#upvotes and Evidence#downvotes

--- spec/models/evidence_spec.rb
+++ spec/models/evidence_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Evidence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # before(:each) do
+  #   @evidence = create(:evidence)
+  #   @upvote = create(:vote)
+  #   @upvote.evidence_id = @evidence.id
+  #   @upvote.upvote = true
+  #   @downvote = create(:vote)
+  #   @downvote.upvote = false
+  # end
+
+  # after(:each) do
+  #   p @upvote
+  #   p @evidence
+  #   p @evidence.upvotes
+  # end
+
+  # it "returns the right amount of upvotes" do
+  #   @upvote.evidence_id = @evidence.id
+  #   expect(@evidence.upvotes).to eq 1
+  #   expect(@evidence.votes.where(upvote: true).count).to eq 1
+  # end
+
+  describe "#upvotes" do
+    it "upvotes increase by 1 when new upvote applied" do
+      evidence = create(:evidence)
+      create(:vote, upvote: true, evidence_id: evidence.id)
+      expect(evidence.upvotes).to eq 1
+    end
+  end
+
+  describe "#downvotes" do
+    it "downvotes increase by 1 when new downvote applied" do
+      evidence = create(:evidence)
+      create(:vote, upvote: false, evidence_id: evidence.id)
+      expect(evidence.downvotes).to eq 1
+    end
+  end
+
 end
+
