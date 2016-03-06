fad2d4c4865fe019a8d6bc700b4da2aa283d3dc0
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-05 22:31:01 -0500

    add factory to create new evidence object

--- spec/factories/evidences.rb
+++ spec/factories/evidences.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
+  # factory :evidence do
+  #   url "https://www.test.com"
+  #   support true
+  #   user_id 1
+  #   fact_id 1
+  # end
+
   factory :evidence do
-    
+    url "https://www.test.com"
   end
+
 end
