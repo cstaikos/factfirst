9b378f1af977fa837ec5b980c435b6b3d0873718
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-05 22:34:08 -0500

    write tests for fact model methods #set_defaults, #supporting_evidence, #refuting_evidence, and #update_score

--- spec/models/fact_spec.rb
+++ spec/models/fact_spec.rb
@@ -1,23 +1,68 @@
 require 'rails_helper'
-# require 'spec_helper'
+
 
 RSpec.describe Fact, type: :model do
 
-  before(:each) do
-    @fact = create(:fact)
-  end
-
-
-  it "has a default score of 0 " do
-    expect(@fact.score).to eq 0
-  end
-
-
-
-
-  # it "has been created properly" do
-  #   expect(@fact.body).to eq "Test Fact"
+  # before(:each) do
+  #   # Fact.delete_all
+  #   @fact = create(:fact)
+  #   @evidence = create(:evidence)
+  #   @vote = create(:vote)
   # end
+
+  after(:all) do
+    Fact.delete_all
+    Evidence.delete_all
+    p Fact.all
+    p Evidence.all
+  end
+
+  #
+  # it "has has properties defined in factory" do
+  #   expect(Fact.count).to eq 1
+  # end
+  #
+  # it "has a higher score when associated evidence is upvoted" do
+  #   @evidence.fact_id = @fact.id
+  #   @evidence.support = true
+  #   @vote.evidence_id = @evidence.id
+  #   @vote.upvote = true
+  #   expect(@fact.score).to eq 1
+  # end
+
+  describe "#set_defaults" do
+    it "sets the default fact score to 0" do
+      fact = create(:fact)
+      expect(fact.score).to eq 0
+    end
+  end
+
+  describe "#supporting_evidence" do
+    it "increase fact supporting evidence collection by 1" do
+      fact = create(:fact)
+      evidence = create(:evidence, support: true, fact_id: fact.id)
+      expect(fact.supporting_evidence[0]).to eq evidence
+    end
+  end
+
+  describe "#refuting_evidence" do
+    it "decreases fact supporting evidence collection by 1" do
+      fact = create(:fact)
+      evidence = create(:evidence, support: false, fact_id: fact.id)
+      expect(fact.refuting_evidence[0]).to eq evidence
+    end
+  end
+
+  describe "#update_score" do
+    it "verifies that 1 upvote for refuting evidence and 1 upvote for supporting evidence results in a score of 50" do
+      fact = create(:fact)
+      evidence_1 = create(:evidence, support: true, fact_id: fact.id)
+      evidence_2 = create(:evidence, support: false, fact_id: fact.id)
+      vote_1 = create(:vote, upvote: true, evidence_id: evidence_1.id)
+      vote_2 = create(:vote, upvote: true, evidence_id: evidence_2.id)
+      expect(fact.update_score).to eq 50
+    end
+  end
 
 end
 
@@ -28,9 +73,5 @@ end
 
 
 
-# it "creates a new fact" do
-#   user = User.create
-#   fact = Fact.create
-#   expect(fact.user_id).to eq(user.id)
-# end
+
 
