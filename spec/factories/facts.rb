9ada211df2b394fc63f2914822632a6a64850b52
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-05 22:32:09 -0500

    add factory to create new fact and vote objects(lets consider creating a votes model and factory)

--- spec/factories/facts.rb
+++ spec/factories/facts.rb
@@ -1,10 +1,16 @@
 FactoryGirl.define do
 
+  # after(:create) { |object| puts "Created a new #{object}" }
+
   factory :fact do
     body "Test Fact"
   end
 
-end
+  factory :vote do
+    upvote true
+  end
 
+
+end
 
 
