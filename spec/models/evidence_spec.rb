4cf1c5b5ee6778f4dac307ebff8dccaa7f6b0428
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-07 19:07:02 -0500

    refactor evidence_spec.rb

--- spec/models/evidence_spec.rb
+++ spec/models/evidence_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Evidence, type: :model do
 
+  let!(:evidence) { create(:evidence) }
 
   describe "#upvotes" do
     context "when adding an upvote to evidence" do
       it "how many upvotes the peice of evidence has(should be 1 in this case)" do
-        evidence = create(:evidence)
         create(:vote, upvote: true, evidence_id: evidence.id)
         expect(evidence.upvotes).to eq 1
       end
@@ -16,7 +16,6 @@ RSpec.describe Evidence, type: :model do
   describe "#downvotes" do
     context "when adding a downvote to evidence" do
       it "returns evidence downvotes count including the newly created downvote" do
-        evidence = create(:evidence)
         create(:vote, upvote: false, evidence_id: evidence.id)
         expect(evidence.downvotes).to eq 1
       end
