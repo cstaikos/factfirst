963cf1580e650625b898ad8d5397b0f5de16a520
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-07 21:57:11 -0500

    fix #flash-messages fadeout bug

--- app/assets/javascripts/flashnotice.js
+++ app/assets/javascripts/flashnotice.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function(){
     //$('#flash-messages').parent().append($('#flash-messages'));
-    $('#flash-messages').delay(5000).fadeOut(400)
+    $('#flash-messages').delay(5000).fadeOut(400);
 
     $('#flash-messages').on('click', function(e) {
     e.preventDefault();
