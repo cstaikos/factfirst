51bde6e6a34a7c3e7075095316f610bfeb3968f7
Filip Stankovic <filips@gmail.com>
2016-03-08 17:05:51 -0500

    Style star next to fact info indicating favorite / non-favorite fact

--- app/helpers/facts_helper.rb
+++ app/helpers/facts_helper.rb
@@ -3,12 +3,12 @@ module FactsHelper
   def favorite_button (fact)
 
     if !current_user || current_user.favorites.exists?(fact) == false
-      link_to 'WATCH',
+      link_to content_tag(:i, "", class: "fa fa-star-o unfav"),
               create_favorite_path(fact),
               method: :post,
               remote: !!current_user
     else
-      link_to 'UNWATCH',
+      link_to content_tag(:i, "", class: "fa fa-star fav"),
               destroy_favorite_path(fact),
               method: :delete,
               remote: true
