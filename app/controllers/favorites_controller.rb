51da5ad486add2278737b1e14b204d4209b9443a
Chris Staikos <cstaikos@gmail.com>
2016-03-07 20:56:01 -0500

    Favorites controller actions - destroy, create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,37 @@
 class FavoritesController < ApplicationController
+
+  before_action :check_login
+
   def create
-    
+    @fact = Fact.find(params[:id])
+    @user = current_user
+
+    @user.favorites << @fact
+    @user.save
+
+    respond_to do |format|
+      format.js {}
+      format.html { redirect_to @fact }
+    end
+
   end
 
   def destroy
+    @fact = Fact.find(params[:id])
+    @user = current_user
 
+    @user.favorites.delete(@fact)
+
+    respond_to do |format|
+      format.js {}
+      format.html { redirect_to @fact }
+    end
   end
+
+  private
+
+  def check_login
+    redirect_to new_user_session_path unless current_user
+  end
+
 end
