a4c16dc403f10d2a3d32429243a064df7925c4ee
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-08 17:04:53 -0500

    fix nav-bar display issue on login page (when navigating to from home page)

--- app/assets/javascripts/refills/navigation.js
+++ app/assets/javascripts/refills/navigation.js
@@ -1,6 +1,7 @@
 // Add nice transition into jQuery "hamburger" nav menu
 $(document).on("ready page:load", function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
+    $('.navigation').off();
+    var menuToggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
 
   menuToggle.on('click', function(e) {
