30012769b413d0009b7cced543b55bac2234f3e7
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-08 23:55:39 -0500

    fix footer bug - now it works on all pages

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,14 +1,22 @@
 $(document).on('ready', function(){
+    var formHeight = $('.form-wrapper').height();
+    var textWrapHeight = $('.text-wrapper').height();
 
+    //Accounts for pages with very little content
+    if(textWrapHeight < 800){
+        $('.text-wrapper').css('height', '90vh');
+    }
+
+    //Handles the special case of nested forms on the add fact page
     $('.links').on('click', function(){
 
-        var formHeight = $('.form-wrapper').height();
-        var textWrapHeight = $('.text-wrapper').height();
+        formHeight = $('.form-wrapper').height();
+        textWrapHeight = $('.text-wrapper').height();
 
         if ( formHeight > 500){
             $('.text-wrapper').css('height', '');
         } else if (textWrapHeight < 800 && formHeight < 900) {
-            $('.text-wrapper').css('height', '90vh');
+            //$('.text-wrapper').css('height', '90vh');
         }
 
     });
