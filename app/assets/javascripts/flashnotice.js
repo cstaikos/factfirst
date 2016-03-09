b72ce1c3b0bed021bc64ed7be3bc7059bb29ebda
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-08 22:47:55 -0500

    fix flash display placement on landing page

--- app/assets/javascripts/flashnotice.js
+++ app/assets/javascripts/flashnotice.js
@@ -1,9 +1,24 @@
 $(document).on('ready page:load', function(){
-    //$('#flash-messages').parent().append($('#flash-messages'));
+
+
     $('#flash-messages').delay(5000).fadeOut(400);
 
     $('#flash-messages').on('click', function(e) {
     e.preventDefault();
     $( this ).remove();
   });
+
+    var path = window.location.pathname
+
+    if (path === '/'){
+        $('#flash-messages').css({
+            top: 5,
+            width: '35%',
+            left: 0,
+            right: 0,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        })
+
+    }
 });
