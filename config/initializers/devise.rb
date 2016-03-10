36e847c9eb7bb007c70557c5ad3cfa11bad485cd
Chris Staikos <cstaikos@gmail.com>
2016-03-10 15:54:31 -0500

    Set up different seed files for different environments

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '1146ecd13ef66bb7c592dd4ab724cde8a919f8e3d3c11a965099af2d125236219cceb4484afcb861d7b99944b04c6813e7ffbabdd69a3365f13f47a5e7e3bab4'
   # config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
-
+  
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
