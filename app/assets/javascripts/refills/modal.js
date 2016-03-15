9b4532e7e3bda7b229106c180e6a22a26269a661
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-15 17:09:47 -0400

    write script for #modal-2

--- app/assets/javascripts/refills/modal.js
+++ app/assets/javascripts/refills/modal.js
@@ -15,3 +15,21 @@ $(function() {
     e.stopPropagation();
   });
 });
+
+$(function() {
+    $("#modal-2").on("change", function() {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-open");
+        } else {
+            $("body").removeClass("modal-open");
+        }
+    });
+
+    $(".modal-window").on("click", function() {
+        $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+        e.stopPropagation();
+    });
+});
