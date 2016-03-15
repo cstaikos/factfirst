c5c4752c86e00d54a18df92be7ec22002e51875c
Chris Staikos <cstaikos@gmail.com>
2016-03-15 16:19:15 -0400

    Fix instant search consistency (wasn't grabbing last char)

--- app/assets/javascripts/facts.js
+++ app/assets/javascripts/facts.js
@@ -5,7 +5,7 @@ $(document).on('ready page:load', function(){
   }, 10)
 
 
-  $("#search-index").on('keydown', function() {
+  $("#search-index").on('keyup', function() {
     if ($(this).val().length > 1) {
       $.ajax({
         url: '/facts?query=' + encodeURIComponent( $(this).val() ),
