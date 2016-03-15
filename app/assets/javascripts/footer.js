3b2d6c882dfda377d74522e1122a69cba40db65c
Chris Staikos <cstaikos@gmail.com>
2016-03-15 16:30:21 -0400

    Make footer height adjustment work on all pages

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,6 +1,19 @@
+var fixFooterHeight = function() {
+  var formHeight = $('.form-wrapper').height();
+  var textWrapHeight = $('.application-wrapper').height();
+
+  //Accounts for pages with very little content
+  if(textWrapHeight < 800){
+      $('.application-wrapper').css('height', '90vh');
+  }
+}
+
 $(document).on('ready', function(){
 
-
+    // If no muut forum, fix footer height on doc ready
+    if (typeof muut == "undefined") {
+      fixFooterHeight();
+    }
     //Handles the special case of nested forms on the add fact page
     $('.links').on('click', function(){
 
@@ -17,12 +30,9 @@ $(document).on('ready', function(){
 
 });
 
-muut(function() {
-  var formHeight = $('.form-wrapper').height();
-  var textWrapHeight = $('.application-wrapper').height();
-
-  //Accounts for pages with very little content
-  if(textWrapHeight < 800){
-      $('.application-wrapper').css('height', '90vh');
-  }
-});
+// If muut forum is on page, wait til it's done loading before setting footer height
+if (typeof muut != "undefined") {
+  muut(function() {
+    fixFooterHeight();
+  });
+}
