aed8f6929778ecc9dcdeef3fded10946ae435fb1
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-15 16:32:05 -0400

    set up /views/users/sessions/create.js.erb to pass flash message into #flash-messages div with jQuery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,7 +34,12 @@ class SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
+    # set_flash_message! :notice, :signed_out if signed_out
+
+    if signed_out
+      flash[:notice] = "Successfully Signed out!"
+    end
+
     yield if block_given?
     respond_to_on_destroy
   end
