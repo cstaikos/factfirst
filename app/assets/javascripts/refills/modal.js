c75ab1e36b0cd6971283dc770c1173af37975d6f
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-16 16:25:43 -0400

    remove unused code from modal.js

--- app/assets/javascripts/refills/modal.js
+++ app/assets/javascripts/refills/modal.js
@@ -1,22 +1,5 @@
 $(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
 
-  $(".modal-window").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
-
-$(function() {
     $("#auth-modal").on("change", function() {
         if ($(this).is(":checked")) {
             $("body").addClass("modal-open");
