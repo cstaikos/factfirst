ae9a686b64cf0dcf7c90875b854e7d82f2ef9cc3
Chris Staikos <cstaikos@gmail.com>
2016-03-16 22:16:59 -0400

    Refactor metadata grabbing so it lives in the evidence model instead of the controller

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -5,36 +5,19 @@ class EvidencesController < ApplicationController
     @fact = Fact.find(params[:fact_id])
     @evidence = @fact.evidences.build(evidence_params)
     @evidence.user = current_user
+    @evidence.grab_metadata
 
     # If user already has 5 evidences on this fact, they can't add more
     if @fact.evidences.where(user_id: current_user).count >= 5
       redirect_to fact_path(@fact), alert: "Evidence not added. You are only permitted 5 evidence submissions per fact!" and return
     end
 
-
-    # Grab metadata from evidence url - 5 second limit on request, otherwise save empty title/description
-    begin
-      timeout(5) do
-        doc = Nokogiri::HTML(open(@evidence.url))
-
-        og_title = doc.css("meta[property='og:title']")
-        og_description = doc.css("meta[property='og:description']")
-
-        @evidence.title = og_title.first.attributes["content"] if og_title.length > 0
-        @evidence.description = og_description.first.attributes["content"] if og_description.length > 0
-      end
-    rescue Timeout::Error
-      puts 'Timeout error'
-    end
-
-
-
-
     if @evidence.save
       # Auto upvote submitted evidence
       @evidence.votes.create(upvote: true, user: current_user)
       @evidence.fact.update_score
 
+      # Create new source or grab existing one if it exists
       new_source = Source.create_from_url(@evidence.url)
       if new_source
         @evidence.source = new_source
