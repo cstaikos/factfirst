94d71407276c5f0a9787c55f69c3ce0a8713fbd9
Chris Staikos <cstaikos@gmail.com>
2016-03-16 20:26:17 -0400

    Add wot_trust and wot_confidence to source table

--- app/models/source.rb
+++ app/models/source.rb
@@ -7,7 +7,8 @@ class Source < ActiveRecord::Base
 
   end
 
-  def get_wot_score
+  def get_wot
+    url = "http://api.mywot.com/0.4/public_link_json2?hosts=wired.com/&key=#{ENV['WOT_KEY']}"
 
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316182329) do
+ActiveRecord::Schema.define(version: 20160317002443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,9 +59,10 @@ ActiveRecord::Schema.define(version: 20160316182329) do
 
   create_table "sources", force: :cascade do |t|
     t.string   "domain"
-    t.integer  "wot_score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "wot_trust"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "wot_confidence"
   end
 
   create_table "users", force: :cascade do |t|
