fb245d1adfb4a5e6dc80055016eb2fe48da36017
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-17 00:04:20 -0400

    css tweaks and javascript experiments - nothing solid

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,6 +54,7 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Added for facebook oauth
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -63,6 +64,7 @@ class User < ActiveRecord::Base
     end
   end
 
+  #Add for facebook oauth
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
