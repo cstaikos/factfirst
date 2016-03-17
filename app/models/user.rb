e2a849a7104e275824bd73c70f8be2cd3a0c947d
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-16 21:41:03 -0400

    add #find_for_google_oauth2 to user model - and add [:google_oauth2] to the omniauthprovider keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,  :omniauthable, :omniauth_providers => [:google]
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :omniauthable, :omniauth_providers =>  [:google_oauth2]
 
   has_many :facts
   has_many :evidences
@@ -27,5 +32,27 @@ class User < ActiveRecord::Base
     upvote_total = evidence_votes.inject(0) { |sum, vote| sum += (vote.upvote ? 1 : 0) }
     ((upvote_total.to_f / evidence_votes.count) * 100).round(2)
   end
+
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+                           provider:access_token.provider,
+                           email: data["email"],
+                           uid: access_token.uid ,
+                           password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+  end
+
 
 end
