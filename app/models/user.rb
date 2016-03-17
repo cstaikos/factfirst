b1099e9c21a40cead5297506a6713c8107cdbeea
Chris Staikos <cstaikos@gmail.com>
2016-03-17 13:53:05 -0400

    Fix bug where oauth tries to assign a val to a user's 'name' - should be display_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
       if registered_user
         return registered_user
       else
-        user = User.create(name: data["name"],
+        user = User.create(display_name: data["name"],
                            provider:access_token.provider,
                            email: data["email"],
                            uid: access_token.uid ,
