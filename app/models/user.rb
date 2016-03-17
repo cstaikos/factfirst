2ad867b57b096537ed2257acdb4420e9e3951243
Chris Staikos <cstaikos@gmail.com>
2016-03-17 13:43:40 -0400

    Fix bug where FB auth didn't work

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
   end
 
 
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+  def self.find_for_oauth(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
     if user
