67b300d2c2e19456c48ebf1b6388b3741733a70c
Chris Staikos <cstaikos@gmail.com>
2016-03-16 20:49:37 -0400

    Use WOT api to get trust and confidence scores whenever a new source is created

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,14 +1,19 @@
 class Source < ActiveRecord::Base
   has_many :evidences
 
-  after_create :set_defaults
+  after_create :get_wot
 
-  def set_defaults
-
-  end
+  require 'httparty'
 
   def get_wot
-    url = "http://api.mywot.com/0.4/public_link_json2?hosts=wired.com/&key=#{ENV['WOT_KEY']}"
+    response = HTTParty.get("http://api.mywot.com/0.4/public_link_json2?hosts=#{domain}/&key=#{ENV['wot_key']}")
+    json = JSON.parse(response.body)
+
+    scores = json[domain]['0']
+
+    self.wot_trust = scores[0].to_i
+    self.wot_confidence = scores[1].to_i
+    self.save
 
   end
 
