52b66c180fbc43235b29a3b7efaed45ed73c2ec2
Chris Staikos <cstaikos@gmail.com>
2016-03-16 21:51:49 -0400

    Assign default wot scores if nothing comes back from api (neutral scores)

--- app/models/source.rb
+++ app/models/source.rb
@@ -5,14 +5,20 @@ class Source < ActiveRecord::Base
 
   require 'httparty'
 
+  # Use web of trust (wot) api to get trust and confidence scores for domain
   def get_wot
     response = HTTParty.get("http://api.mywot.com/0.4/public_link_json2?hosts=#{domain}/&key=#{ENV['wot_key']}")
     json = JSON.parse(response.body)
 
     scores = json[domain]['0']
-
-    self.wot_trust = scores[0].to_i
-    self.wot_confidence = scores[1].to_i
+    if scores
+      self.wot_trust = scores[0].to_i
+      self.wot_confidence = scores[1].to_i
+    else
+      # If no wot data is returned, give a baseline of 50% which will have a neutral effect
+      self.wot_trust = 50
+      self.wot_confidence = 100
+    end
     self.save
 
   end
