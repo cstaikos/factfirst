814c36ac68caefd0ea257b56e2e04e047adb186b
Chris Staikos <cstaikos@gmail.com>
2016-03-19 13:27:22 -0400

    Only call highCharts on user show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,8 @@ $(document).on('ready', function(){
     $('.requires-login').on('click', function(e){
         if ($(this).hasClass('requires-login')) {
           e.preventDefault();
-          $("body").addClass("modal-open");
-          $('#auth-modal').prop("checked", "true");
+          $(".modal-trigger").click();
         }
     });
 
 });
-
-
-
-
