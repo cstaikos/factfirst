6e1758ed9d2fac65b7920dd1415fe3ebb42b1e79
Filip Stankovic <filips@gmail.com>
2016-03-20 16:36:42 -0400

    Setup Truthiness component and render static string using React

---
+++ app/assets/javascripts/components/_truthiness.js.jsx
@@ -0,0 +1,29 @@
+var TruthinessScore = React.createClass({
+  loadScoreFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadScoreFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      React.createElement('span', {},
+        "52"
+      )
+    );
+  }
+});
