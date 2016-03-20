fd8f3e2d3c62d0278ef767e630b474319a2892f9
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-20 01:50:49 -0400

    add user model tests for #evidence_quality

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     display_name "test_user"
     email "test@test.com"
-    password "boogers"
-    password_confirmation "boogers"
+    password "boogerss"
+    password_confirmation "boogerss"
   end
 end
