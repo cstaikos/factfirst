559ec0de2e3093ac0c36e690412bf4a44888e660
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-20 01:51:28 -0400

    add more tests for fact model #update_score

--- spec/models/fact_spec.rb
+++ spec/models/fact_spec.rb
@@ -31,18 +31,82 @@ RSpec.describe Fact, type: :model do
     end
   end
 
+  describe "#total_votes" do
+    context "when there are votes on a facts evidence" do
+      it "returns total votes on a facts evidence" do
+        evidence_1 = create(:evidence, support: true, fact_id: fact.id)
+        evidence_2 = create(:evidence, support: false, fact_id: fact.id)
+        vote_1 = create_list(:vote, 5, upvote: true, evidence_id: evidence_1.id)
+        vote_2 = create_list(:vote, 5, upvote: false, evidence_id: evidence_2.id)
+
+        fact.reload
+        expect(fact.total_votes).to eq 10
+
+      end
+    end
+
+  end
+
   describe "#update_score" do
+
     context "when there are equal upvotes for supporting and refuting evidence" do
       it "returns a fact score of 50" do
         evidence_1 = create(:evidence, support: true, fact_id: fact.id)
         evidence_2 = create(:evidence, support: false, fact_id: fact.id)
         vote_1 = create(:vote, upvote: true, evidence_id: evidence_1.id)
         vote_2 = create(:vote, upvote: true, evidence_id: evidence_2.id)
-        fact.reload # This is necessary to make the test pass. Why?
+
+        fact.reload # This is necessary to make the test pass. Why exactly?
+                    # Because the fact was created before the test, and doesn't know
+                    # about the evidences and votes created in this test
+
         fact.update_score
         expect(fact.score).to eq 50
       end
     end
+
+    context "when there is only supporting evidence and there are equal upvotes and downvotes" do
+      it "returns a fact score of 50" do
+        evidence_1 = create(:evidence, support: true, fact_id: fact.id)
+        evidence_2 = create(:evidence, support: true, fact_id: fact.id)
+        vote_1 = create(:vote, upvote: true, evidence_id: evidence_1.id)
+        vote_2 = create(:vote, upvote: false, evidence_id: evidence_2.id)
+
+        fact.reload
+
+        fact.update_score
+        expect(fact.score).to eq 50
+      end
+    end
+
+    context "when there is only refuting evidence and only upvotes" do
+      it "returns a fact score of 0" do
+        evidence_1 = create(:evidence, support: false, fact_id: fact.id)
+        evidence_2 = create(:evidence, support: false, fact_id: fact.id)
+        vote_1 = create(:vote, upvote: true, evidence_id: evidence_1.id)
+        vote_2 = create(:vote, upvote: true, evidence_id: evidence_2.id)
+
+        fact.reload
+
+        fact.update_score
+        expect(fact.score).to eq 0
+      end
+    end
+
+    context "when there is only refuting evidence and only downvotes" do
+      it "returns a fact score of 0" do
+        evidence_1 = create(:evidence, support: false, fact_id: fact.id)
+        evidence_2 = create(:evidence, support: false, fact_id: fact.id)
+        vote_1 = create(:vote, upvote: true, evidence_id: evidence_1.id)
+        vote_2 = create(:vote, upvote: true, evidence_id: evidence_2.id)
+
+        fact.reload
+
+        fact.update_score
+        expect(fact.score).to eq 0
+      end
+    end
+
   end
 
 end
