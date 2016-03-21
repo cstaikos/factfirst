4047d837b2022f3a45aa51731a28d1fd0cdd462f
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-20 21:49:28 -0400

    add calculations for fact score average chart in users#metrics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController < ApplicationController
     @facts_by_category_count = []
     @votes_by_category_count = []
 
+    @total_evidence_upvotes = @user.evidences.inject(0) {|sum, evidence| sum + evidence.upvotes}
+    @total_evidence_downvotes = @user.evidences.inject(0) {|sum, evidence| sum + evidence.downvotes}
+    @total_votes = @total_evidence_downvotes + @total_evidence_upvotes
+    @evidence_quality = @user.evidence_quality
+    @average_fact_score = @user.facts.average(:score).to_f
+
+    @facts_supporting_evidence =@user.facts.inject(0){|sum, fact| sum + fact.evidences.where(support: true).count}
+    @facts_refuting_evidence =@user.facts.inject(0){|sum, fact| sum + fact.evidences.where(support: false).count}
+    # @user.facts.
+
     Category.all.each do |category|
       @facts_by_category_count << category.facts.where(user: @user).count
 
@@ -25,7 +35,14 @@ class UsersController < ApplicationController
         render json: [Category.all.pluck('name'),
                       @facts_by_category_count,
                       @evidences_by_category_count,
-                      @votes_by_category_count]
+                      @votes_by_category_count,
+                      @total_evidence_upvotes,
+                      @total_evidence_downvotes,
+                      @evidence_quality,
+                      @average_fact_score,
+                      @facts_supporting_evidence,
+                      @facts_refuting_evidence
+                      ]
       end
     end
     
