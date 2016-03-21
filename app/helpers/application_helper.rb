6335d04f9da9872138b72a2c7e3c5f7f2fb9cba6
Chris Staikos <cstaikos@gmail.com>
2016-03-20 20:37:11 -0400

    Partial and view helper in place for meta tags, basic tags in place

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,31 @@ module ApplicationHelper
   def photo_path
     (Rails.env.production? ? "public/imagemagick/fact_photos/" : "public/fact_photos/")
   end
+
+  def controller_action
+    "#{params[:controller]}\##{params[:action]}"
+  end
+
+  def meta_tags
+    tags = {}
+    tags[:description] = "Truthometer enables truth to unfold via a collaborative process of evidence collection and verification."
+
+    case controller_action
+    when "facts#show"
+      tags[:title] = "Fact | Truthometer"
+    when "facts#index"
+      tags[:title] = "Browse Facts | Truthometer"
+    when "facts#new"
+      tags[:title] = "New Facts | Truthometer"
+    when "static_pages#home"
+      tags[:title] = "Truthometer"
+    when "static_pages#about"
+      tags[:title] = "About | Truthometer"
+    when "static_pages#forum"
+      tags[:title] = "Forum | Truthometer"
+    end
+
+    tags
+
+  end
 end
