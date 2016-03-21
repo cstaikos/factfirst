c99f412de419695cb90e1ce3f5ed0818fbe5987e
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-21 15:14:34 -0400

    user_show.scss adjustments and highcharts styling

--- app/assets/javascripts/user_show.js
+++ app/assets/javascripts/user_show.js
@@ -5,21 +5,21 @@
 
         // Set chart options globally
 
-        //Highcharts.setOptions({
-        //  chart: {
-        //    backgroundColor: {
-        //      linearGradient: [0, 0, 500, 500],
-        //      stops: [
-        //        [0, 'rgb(255, 255, 255)'],
-        //        [1, 'rgb(240, 240, 255)']
-        //      ]
-        //    },
-        //    borderWidth: 2,
-        //    plotBackgroundColor: 'rgba(255, 255, 255, .9)',
-        //    plotShadow: true,
-        //    plotBorderWidth: 1
-        //  }
-        //});
+        Highcharts.setOptions({
+          chart: {
+            backgroundColor: {
+              linearGradient: [0, 0, 500, 500],
+              stops: [
+                [0, 'rgb(255, 255, 255)'],
+                [1, 'rgb(240, 240, 255)']
+              ]
+            },
+            borderWidth: 2,
+            plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+            plotShadow: true,
+            plotBorderWidth: 1
+          }
+        });
 
         var activity_by_category_options = ({
           chart: {
